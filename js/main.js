78014d58439154d5b83be008d5487eeb684354a0
Michal P <43756311+michpcx@users.noreply.github.com>
2019-05-16 00:01:19 +0100

    Addictional improvements

--- js/main.js
+++ js/main.js
@@ -5,35 +5,41 @@ $( document ).ready(function() {
     setInterval(Item4, 12000);
     setInterval(Item5, 16000);
     setInterval(Item6, 20000);
+    setInterval(Item7, 24000);
 });
 
 
 function Item1()
 {
-  $("#listItem1").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem1").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
 
 function Item2()
 {
-  $("#listItem2").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem2").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
 
 function Item3()
 {
-  $("#listItem3").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem3").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
 
 function Item4()
 {
-  $("#listItem4").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem4").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
 
 function Item5()
 {
-  $("#listItem5").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem5").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
 
 function Item6()
 {
-  $("#listItem6").fadeIn(1000).delay(2000).fadeOut(950).delay(20000);
+  $("#listItem6").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
+}
+
+function Item7()
+{
+  $("#listItem7").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
 }
