660c53529fbe70b3f6655674567df15cc70aefad
Michal P <43756311+michpcx@users.noreply.github.com>
2019-06-29 15:27:20 +0100

    Fixed timings on the interest list

--- js/main.js
+++ js/main.js
@@ -1,45 +1,28 @@
 $( document ).ready(function() {
-    setInterval(Item1, 0);
-    setInterval(Item2, 4000);
-    setInterval(Item3, 8000);
-    setInterval(Item4, 12000);
-    setInterval(Item5, 16000);
-    setInterval(Item6, 20000);
-    setInterval(Item7, 24000);
+
+  function mainLoop(){
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("travelling."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("war strategy."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("game making."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("politics."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("video games."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("history."); $(this).dequeue()})
+          .animate({opacity:1});
+      $("#listItem").delay(2000).animate({opacity:0}, 500)
+          .queue(function(){$(this).text("space exploration."); $(this).dequeue()})
+          .animate({opacity:1}, mainLoop);
+    }
+
+    mainLoop();
 });
-
-
-function Item1()
-{
-  $("#listItem1").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item2()
-{
-  $("#listItem2").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item3()
-{
-  $("#listItem3").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item4()
-{
-  $("#listItem4").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item5()
-{
-  $("#listItem5").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item6()
-{
-  $("#listItem6").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
-
-function Item7()
-{
-  $("#listItem7").fadeIn(1000).delay(2000).fadeOut(950).delay(24000);
-}
