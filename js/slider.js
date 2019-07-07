c62ebc88f545bbdde2272f54bfd08ab7c167471e
Michal P <43756311+michpcx@users.noreply.github.com>
2019-07-07 18:17:12 +0100

    Completely redone travel page

---
+++ js/slider.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+
+    var bgArray = ['2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg'];
+    var arrChecker = new Array();
+
+
+
+    // If you have defined a path for the images
+    var path = 'assets/backImages/';
+
+    var bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    arrChecker.push(bg);
+    $('#image2').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image3').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image4').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image5').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image6').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image7').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image8').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image9').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image10').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image11').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image12').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image13').css('background-image', 'url('+path+bg+')');
+
+    bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+    if (arrChecker.includes(bg)) {
+      do {
+          bg = bgArray[Math.floor(Math.random() * bgArray.length)];
+        }
+        while (arrChecker.includes(bg));
+    }
+    $('#image14').css('background-image', 'url('+path+bg+')');
+
+
+});
