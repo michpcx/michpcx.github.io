a937720009cd50ac6352ebeeea3f790b569470ad
Michal P <michpcx@protonmail.com>
2020-12-16 22:01:13 +0000

    Began work on portfolio section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $( document ).ready(function() {
 
+    // isotope
+    $('.gallery').isotope({
+      // options
+      itemSelector: '.items'
+    });
+
+    var $gallery = $('.gallery').isotope({
+      // options
+    });
+
+    // filter items on button click
+    $('.filtering').on( 'click', 'span', function() {
+
+        var filterValue = $(this).attr('data-filter');
+
+        $gallery.isotope({ filter: filterValue });
+
+    });
+
+    $('.filtering').on( 'click', 'span', function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
+
+    });
+
 
 
     function mainLoopPL(){
@@ -26,40 +51,40 @@ $( document ).ready(function() {
             .animate({opacity:1}, mainLoopPL);
       }
 
-            var words = document.querySelectorAll(".word");
-        words.forEach(function (word) {
-            var letters = word.textContent.split("");
-            word.textContent = "";
-            letters.forEach(function (letter) {
-                var span = document.createElement("span");
-                span.textContent = letter;
-                span.className = "letter";
-                word.append(span);
-            });
+    var words = document.querySelectorAll(".word");
+    words.forEach(function (word) {
+    var letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach(function (letter) {
+        var span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+    });
+    var currentWordIndex = 0;
+    var maxWordIndex = words.length - 1;
+    words[currentWordIndex].style.opacity = "1";
+    var rotateText = function () {
+        var currentWord = words[currentWordIndex];
+        var nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+        // rotate out letters of current word
+        Array.from(currentWord.children).forEach(function (letter, i) {
+            setTimeout(function () {
+                letter.className = "letter out";
+            }, i * 80);
         });
-        var currentWordIndex = 0;
-        var maxWordIndex = words.length - 1;
-        words[currentWordIndex].style.opacity = "1";
-        var rotateText = function () {
-            var currentWord = words[currentWordIndex];
-            var nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-            // rotate out letters of current word
-            Array.from(currentWord.children).forEach(function (letter, i) {
-                setTimeout(function () {
-                    letter.className = "letter out";
-                }, i * 80);
-            });
-            // reveal and rotate in letters of next word
-            nextWord.style.opacity = "1";
-            Array.from(nextWord.children).forEach(function (letter, i) {
-                letter.className = "letter behind";
-                setTimeout(function () {
-                    letter.className = "letter in";
-                }, 340 + i * 80);
-            });
-            currentWordIndex =
-                currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-        };
-        rotateText();
-        setInterval(rotateText, 4000);
+        // reveal and rotate in letters of next word
+        nextWord.style.opacity = "1";
+        Array.from(nextWord.children).forEach(function (letter, i) {
+            letter.className = "letter behind";
+            setTimeout(function () {
+                letter.className = "letter in";
+            }, 340 + i * 80);
+        });
+        currentWordIndex =
+            currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+    };
+    rotateText();
+    setInterval(rotateText, 4000);
 });
